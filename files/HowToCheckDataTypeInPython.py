594f47b1df95404151b501a70d63dd75ceda735b
casedigital <casedigital33@gmail.com>
2021-12-23 21:04:37 -0700

    How To Check Data Type In Python

---
+++ files/HowToCheckDataTypeInPython.py
@@ -0,0 +1,15 @@
+# How To Check Data Type In Python?
+# https://youtu.be/Xdo8N8onpTs
+
+
+if __name__ == '__main__':
+
+    my_int = 5
+    my_float = 5.5
+    my_str = "Subscribe to Case Digital!"
+    my_bool = True
+    my_lst = [1, 3, 4]
+    my_dict = {"Hello": "World"}
+    my_tup = (1, 2, 3)
+
+    print(type(my_lst) == list)
