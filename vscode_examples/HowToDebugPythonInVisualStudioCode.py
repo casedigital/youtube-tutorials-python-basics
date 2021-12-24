146cc6f5c5f3c84fd4f533e36bb95c1a390916c3
casedigital <casedigital33@gmail.com>
2021-12-23 20:59:49 -0700

    How to debug python in visual studio code

---
+++ vscode_examples/HowToDebugPythonInVisualStudioCode.py
@@ -0,0 +1,13 @@
+# How To Debug Python In Visual Studio Code?
+# https://youtu.be/IQzqPHP51R0
+# https://stackoverflow.com/questions/38923386/add-breakpoint-in-visual-studio-code
+
+
+def test_function(my_str):
+    my_str += ' Subscribe to Case Digital!'
+    return my_str
+
+
+if __name__ == '__main__':
+    
+    print(test_function('You should'))
