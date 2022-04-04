6b2586d5fb3034e61e45beb1300d24dec46a07a1
casedigital <casedigital33@gmail.com>
2022-04-03 19:37:43 -0600

    How To Convert Decimal To Binary In Python

---
+++ datatypes/HowToConvertDecimalToBinaryInPython.py
@@ -0,0 +1,8 @@
+# How To Convert Decimal To Binary In Python
+# https://youtu.be/-bXmMvlMQIk
+
+if __name__ == "__main__":
+    print("Subscribe to Case Digital")
+    num = 7  # 0111
+    binary_num = bin(num)
+    print(f"Decimal = {num}   Binary = {binary_num[2:]}")
