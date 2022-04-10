7fb02e1830bfbf0cdfddade2a8f66de4e1ec5215
casedigital <casedigital33@gmail.com>
2022-04-10 16:08:50 -0600

    Added youtube link

--- datatypes/HowToConvertBooleanToIntInPython.py
+++ datatypes/HowToConvertBooleanToIntInPython.py
@@ -1,4 +1,5 @@
 # How to convert boolean to int in python?
+# https://youtu.be/TNAx1RH8Onw
 
 
 if __name__ == "__main__":
@@ -6,5 +7,5 @@ if __name__ == "__main__":
     is_the_worst = False
 
     print(should_subscribe, int(should_subscribe))
-    print(f'{should_subscribe = }  {int(should_subscribe)}')
-    print(f'{is_the_worst = }  {int(is_the_worst)}')
+    print(f"{should_subscribe = }  {int(should_subscribe)}")
+    print(f"{is_the_worst = }  {int(is_the_worst)}")
