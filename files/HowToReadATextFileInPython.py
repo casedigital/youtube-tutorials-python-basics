789062ebd77b5754e04c9ed270b4d4f942713945
casedigital <casedigital33@gmail.com>
2022-04-14 01:01:31 -0600

    How To Read A text File In Python

---
+++ files/HowToReadATextFileInPython.py
@@ -0,0 +1,21 @@
+# How to read a text file in python?
+
+
+if __name__ == "__main__":
+    # Method 1
+    cur_file = open("./MyTextFile.txt", "r")
+    contents = cur_file.read()
+    print(contents)
+    cur_file.close()
+
+    print("-" * 80)
+    # Method 2
+    with open("./MyTextFile.txt", "r") as stream:
+        contents = stream.readlines()
+        # contents = [val.strip() for val in contents]
+        new_contents = []
+        for val in contents:
+            new_contents.append(val.strip())
+        print(new_contents)
+    
+    print('Subscribe to Case Digital!')
