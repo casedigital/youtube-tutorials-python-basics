28ba60aa1cd6338d3272486be97de7e3c9e17bf8
casedigital <casedigital33@gmail.com>
2022-06-21 23:57:59 -0600

    How to make a random color generator in python

---
+++ random/HowToMakeARandomColorGeneratorInPython.py
@@ -0,0 +1,22 @@
+# How to make a random color generator in python?
+
+import random
+from PIL import Image
+
+def generate_rgb_color() -> tuple:
+    # r, g, b -> 0 - 255
+    r = random.randint(0, 255)
+    g = random.randint(0, 255)
+    b = random.randint(0, 255)
+    return (r, g, b)
+
+
+if __name__ == "__main__":
+
+    color = generate_rgb_color()
+    print("color: ", color)
+
+    img = Image.new("RGB", (200, 200), color)
+    img.show()
+
+
