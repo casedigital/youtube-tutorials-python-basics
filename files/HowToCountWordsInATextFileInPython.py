9555337b01a40d0f254759eb354cc8fe350ca45c
casedigital <casedigital33@gmail.com>
2022-07-02 17:20:27 -0600

    How to count words in a text file in python

---
+++ files/HowToCountWordsInATextFileInPython.py
@@ -0,0 +1,15 @@
+# How to count words in a text file in python?
+
+import os
+
+
+if __name__ == "__main__":
+    path_to_txt = os.path.abspath(os.path.join("./", "MyTextFile.txt"))
+
+    text = ""
+    with open(path_to_txt, "r") as stream:
+        text = stream.read()
+
+    words = text.split()
+    print("Words     : ", words)
+    print("Word Count: ", len(words))
