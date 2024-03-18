ee78ad2a8178c2e54a0ed384b31be8aa34575636
casedigital <casedigital33@gmail.com>
2024-03-17 18:20:22 -0600

    How to make a string repeat in python

---
+++ strings/HowToMakeAStringRepeatInPython.py
@@ -0,0 +1,8 @@
+# How to make a string repeat in python?
+
+if __name__ == "__main__":
+    dashes = '-' * 80
+    print(dashes)
+    hello4 = 'hello my name is Zach' * 4
+    print(hello4 + '!')
+    print(dashes)
